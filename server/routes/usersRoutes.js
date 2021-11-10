a08621cb0c648fc37379950866b2df88e34b5c86
Claudia Font <claudiafontmarques@gmail.com>
2021-11-10 19:46:04 +0100

    added login user controler and login user route

--- server/routes/usersRoutes.js
+++ server/routes/usersRoutes.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const { validate } = require("express-validation");
+const { createUser, loginUser } = require("../controllers/usersController");
 
 const usersSchema = require("../schemas/userSchema");
+
 const router = express.Router();
 
 router.post("/users", validate(usersSchema), createUser);
+router.post("/users", validate(usersSchema), loginUser);
 
 module.exports = router;
