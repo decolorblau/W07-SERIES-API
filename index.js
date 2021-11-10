67a81866fb1c0cf3ca6f6a89081a4d986d27a8ef
Dante Allievi <danteallievi@gmail.com>
2021-11-10 19:37:37 +0100

    Server IFE added

---
+++ index.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const { initializeServer } = require("./server/index");
+const connectDB = require("./database/index");
+
+const PORT = process.env.PORT ?? 5000;
+
+(async () => {
+  try {
+    await connectDB(process.env.DB_STRING);
+    await initializeServer(PORT);
+  } catch (error) {
+    process.exit(1);
+  }
+})();
